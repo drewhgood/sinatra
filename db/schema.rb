1ebb2a5aaca5c611e3d798fcd1cfa647b03a4424
drewhgood <drewhgood@gmail.com>
2015-02-21 23:45:05 +0000

    timestamps added including necessary migration files.

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150221221426) do
+ActiveRecord::Schema.define(version: 20150221233618) do
 
   create_table "messages", force: true do |t|
-    t.string "author"
-    t.string "content"
-    t.string "url"
+    t.string   "author"
+    t.string   "content"
+    t.string   "url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
