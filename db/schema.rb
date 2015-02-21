7a689e88a2416ee560297e45a034489dcac001db
drewhgood <drewhgood@gmail.com>
2015-02-21 21:59:55 +0000

    Created migration for removing title.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150221195547) do
+ActiveRecord::Schema.define(version: 20150221214655) do
 
   create_table "messages", force: true do |t|
-    t.string "title"
     t.string "author"
     t.string "content"
   end
